fff5c964b4fdc6e6863c17af8e448432fc1d3ee1
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2025-03-18 14:06:25 +0100

    fix:  detect reversed version hash since v8 12.1.8

--- assets/analyzer.js
+++ assets/analyzer.js
@@ -1,15 +1,15 @@
-import { versionHash64 } from './hash.js';
+import { versionHash64 } from "./hash.js";
 
 export function checkSignature(magicNumber) {
   return magicNumber >>> 16 === 0xc0de;
 }
 
 export function findVersions(hash) {
-  const candidates = versions.filter(release => release.hash === hash);
+  const candidates = versions.filter((release) => release.hash === hash);
   return {
-    v8: candidates[0]?.v8,
-    node: candidates.filter(v => v.type === 'node'),
-    electron: candidates.filter(v => v.type === 'electron'),
+    v8: [...new Set(candidates.map((v) => v.v8))],
+    node: candidates.filter((v) => v.type === "node"),
+    electron: candidates.filter((v) => v.type === "electron"),
   };
 }
 
@@ -17,32 +17,27 @@ const versions = await hashVersions();
 
 async function hashVersions() {
   const [nodeVersions, electronVersions] = await Promise.all([
-    fetch('https://nodejs.org/dist/index.json').then(res => res.json()),
-    fetch('https://releases.electronjs.org/releases.json').then(res =>
+    fetch("https://nodejs.org/dist/index.json").then((res) => res.json()),
+    fetch("https://releases.electronjs.org/releases.json").then((res) =>
       res.json()
     ),
   ]);
   const versions = [];
 
   nodeVersions.forEach(({ v8, version }) => {
-    const hash = versionHash64(...v8.split('.').map(Number));
-    versions.push({
-      type: 'node',
-      version,
-      v8,
-      hash,
-    });
+    const versionParts = v8.split(".").map(Number);
+    const hash = versionHash64(...versionParts);
+    const reverseHash = versionHash64(...versionParts.reverse());
+    versions.push({ type: "node", version, v8, hash });
+    versions.push({ type: "node", version, v8, hash: reverseHash });
   });
 
   electronVersions.forEach(({ version, v8, node }) => {
-    const hash = versionHash64(...v8.replace(/-.+/, '').split('.').map(Number));
-    versions.push({
-      type: 'electron',
-      version,
-      node,
-      v8,
-      hash,
-    });
+    const versionParts = v8.replace(/-.+/, "").split(".").map(Number);
+    const hash = versionHash64(...versionParts);
+    const reverseHash = versionHash64(...versionParts.reverse());
+    versions.push({ type: "electron", version, node, v8, hash });
+    versions.push({ type: "electron", version, node, v8, hash: reverseHash });
   });
 
   return versions;
